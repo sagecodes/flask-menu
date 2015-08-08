12b3e02748ec73b159ef61b42af4709530631123
Sage Elliott <wildtroll@gmail.com>
2015-08-08 11:56:47 -0700

    add database files created in previous lesson to database: - database_setup.py - lotsofmenu.oy

--- project.py
+++ project.py
@@ -1,12 +1,21 @@
 from flask import Flask
 app = Flask(__name__)
 
+from database_setup import Base, Restaurant, MenuItem
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+# create Session and connect to DB
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
 @app.route('/')
 @app.route('/hello')
-
 def HelloWorld():
     return "Hello World!"
 
 if __name__ == '__main__':
     app.debug = True
-    app.run() #(host = '0.0.0.0', port = 5000)
+    app.run()
