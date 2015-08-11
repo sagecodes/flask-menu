00561534f6ba84424ed8d60030827652de2dc212
Sage Elliott <wildtroll@gmail.com>
2015-08-10 21:59:13 -0700

    Create template for shoiwng menu items:
    templates/menu.html
    
    render and passin in varaibles:

--- project.py
+++ project.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 from database_setup import Base, Restaurant, MenuItem
@@ -16,16 +17,7 @@ session = DBSession()
 def restaurantMenu(restaurant_id):
     restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
     items = session.query(MenuItem).filter_by(restaurant_id = restaurant.id)
-    output = ''
-    for i in items:
-        output += i.name
-        output += '</br>'
-        output += i.price
-        output += '</br>'
-        output += i.description
-        output += '</br>'
-        output += '</br>'
-    return output
+    return render_template("menu.html", restaurant=restaurant, items = items)
 
 @app.route('/restaurants/<int:restaurant_id>/new/')
 def newMenuItem(restaurant_id):
